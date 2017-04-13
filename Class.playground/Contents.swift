0a5759ef0131686c8ec40c28a70e39196374c6d2
maoam724 <maoam.724@gmail.com>
2017-04-13 21:00:14 +0900

    aa

---
+++ Class.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//            クラス定義
+
+class MyClass {
+    //インスタンスプロパティ
+    var msg = "ハロー"
+    //インスタンスメソッド
+    func hello(){
+        print(msg)
+    }
+}
+
+let myObj = MyClass()
+myObj.hello()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -23,83 +23,139 @@ for _ in 1...6 {
 //
 
 
-func twice(num:Double) -> Double {
-    return num * 2
+func bmi(weight kg:Double, height cm:Double) -> Double {
+    if cm == 0 { return -1 }
+    //体重（kg）を身長（m）の２乗で割る
+    var result = kg/pow(cm*0.01,2)
+    //小数点以下１位で四捨五入する
+    result = round(result*10)/10.0
+    return result
 }
 
+let myBMI = bmi(weight: 51.2, height: 162.0)
+print(myBMI)
 
-//
+
+///////////
 
 
-func plus(a:Double,b:Double) -> Double {
-    return a + b
+let numbers = [4,7,2,9]
+let array1 = numbers.map({( v:Int) -> Int in
+    return v*2
+})
+print(array1)
+
+///////////
+
+let array2 = numbers.map{( v:Int) -> Int in
+    return v*2
 }
 
-let ans = plus(6, b:5)
-print(ans)
+//////////
 
+let array3 = numbers.map{v in v*2}
+print(array3)
 
-//
+/////////
 
-func sum(numbers:Double...) -> Double {
-    var total:Double = 0.0
-    for num in numbers {
-        total += num
+let array4 = numbers.map{$0*2}
+print(array4)
+
+/////////
+
+func battery(capacity:Int) -> (Int) -> Int {
+    //静的ローカル変数
+    var stock = capacity
+    //クロージャとして返すuse関数
+    func use(volume:Int) -> Int {
+        if (stock>volume) {
+            stock -= volume
+        } else {
+            stock=0
+        }
+        return stock
     }
-    return total
+    return use
 }
 
-let goukei = sum(5,6,7,8,9)
-print(goukei)
+
+/////////////
+
+let swift = "swiftとは\n\"アマツバメ\"です。"
+print(swift)
+
+//////////////
+
+let stri = "あいうえお12345ABcde(^_^)"
+let num = stri.characters.count
+print(num)
 
 
-//
+/////////////
 
-func greeting(who:String = "お客様") -> String {
-    return who + "、こんにちは"
+let name1 = "海原"
+let name2 = "次郎丸"
+let strin = name1 + name2 + "さん"
+print(strin)
+
+
+//////////////
+
+let week = ["日", "月", "火", "水", "木", "金", "土"]
+var oneweek = "１"
+for day in week {
+    oneweek += day
 }
-
-let greeting1 = greeting("田中様")
-print(greeting1)
-
-let greeting2 = greeting()
-print(greeting2)
+print(oneweek)
 
 
-//
+/////////////
 
-func calc(a:Int, b:Int=1, c:Int=1) -> Int {
-    return a + b*10 + c*100
-}
-
-let ans1 = calc(0)
-let ans2 = calc(0, b:4)
-let ans3 = calc(0, c:3)
-let ans4 = calc(5, b:4, c:3)
-
-print("ans1 = \(ans1)")
-print("ans2 = \(ans2)")
-print("ans3 = \(ans3)")
-print("ans4 = \(ans4)")
+let kakaku:String = "240"
+let kosu:String = "2"
+let kingaku = Int(kakaku)! * Int(kosu)!
+print(kingaku)
 
 
-//
+/////////////
 
-class ViewController: UIViewController {
-    func testResult(kokugo:Int, sugaku:Int, eigo:Int) -> (total:Int, average:Double) {
-        //３科目合計
-        let total = kokugo + sugaku + eigo
-        //３科目平均
-        var ave = Double(total)/3
-        //小数点以下１位で四捨五入
-        ave = round(ave*10)/10
-        return (total, ave)
-    }
-}
+let r = "20"
+let pai = "3.14"
+let menseki = Double(r)! * Double(r)! * Double(pai)!
+print("半径\(r)の面積は\(menseki)")
 
-let result = testResult(80, sugaku: 68, eigo: 72)
-print("合計 \(result.total)")
-print("平均 \(result.average)")
+/////////////
+
+let nakami = 135.5
+let package = 12.0
+let str1 = "内容量：" + (nakami).description + "g"
+let str2 = "総重量：" + (nakami + package).description + "g"
+print(str1)
+print(str2)
+
+/////////////
+
+let kotoba = "Swift入門ノート"
+var start = kotoba.startIndex
+
+start = start.successor()
+let char = kotoba[start]
+print(char)
+
+/////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
